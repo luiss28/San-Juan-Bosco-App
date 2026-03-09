f3705b4ab5466e8a732d7a208c4868ddff90c415
luiss28 <lsanchez@wpi.edu>
2026-03-08 21:33:26 -0400

    Following React Mega Tutorial. Initializing Git.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Header from './components/Header';
+import FeedPage from './pages/FeedPage';
+import LoginPage from './pages/LoginPage';
+import ExplorePage from './pages/ExplorePage';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container fluid className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path = "/" element = {<FeedPage />} />
+          <Route path = "/explore" element = {<ExplorePage />} />
+          <Route path = "/login" element = {<LoginPage />} />
+          <Route path = "*" element = {<Navigate />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
+  )
 }
 
-export default App;
+export default App
