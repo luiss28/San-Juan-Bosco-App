102c70252d497b44b8259d32aca5339e161bd4ad
luiss28 <lsanchez@wpi.edu>
2026-03-08 21:54:31 -0400

    Completed Chapter 4: Routing and Page Navigation.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Header from './components/Header';
 import FeedPage from './pages/FeedPage';
 import LoginPage from './pages/LoginPage';
 import ExplorePage from './pages/ExplorePage';
+import UserPage from './pages/UserPage';
 
 function App() {
-
   return (
     <Container fluid className="App">
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path = "/" element = {<FeedPage />} />
-          <Route path = "/explore" element = {<ExplorePage />} />
-          <Route path = "/login" element = {<LoginPage />} />
-          <Route path = "*" element = {<Navigate />} />
+          <Route path="/" element = {<FeedPage />} />
+          <Route path="/explore" element = {<ExplorePage />} />
+          <Route path="/user/:username" element={<UserPage />} />
+          <Route path="/login" element = {<LoginPage />} />
+          <Route path="*" element = {<Navigate />} />
         </Routes>
       </BrowserRouter>
     </Container>
