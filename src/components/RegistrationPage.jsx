c058158d00d251400f3967888d38b66d793738d8
luiss28 <lsanchez@wpi.edu>
2026-03-11 10:14:07 -0400

    Minor change in RegistrationPage.jsx

--- src/components/RegistrationPage.jsx
+++ src/components/RegistrationPage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Body from './Body';
-import InputField from '../components/InputField';
+import InputField from './InputField';
 import { useNavigate } from 'react-router-dom';
 
 
