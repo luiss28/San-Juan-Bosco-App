835c633005dfa581bcb174d9f0ac8d62201eb6e0
luiss28 <lsanchez@wpi.edu>
2026-05-28 16:00:04 -0400

    Completed a registration page. Unworking and ugly, but skeleton code is present.

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,78 +1,50 @@
-import { useState, useEffect, useRef } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Body from '../components/Body';
-import InputField from '../components/InputField';
+import { useState } from "react";
+import Body from "../components/Body";
 
-export default function RegistrationPage() {
-  const [formErrors, setFormErrors] = useState({});
-  const usernameField = useRef();
-  const emailField = useRef();
+const MemberForm = ({ existingMember = {}, updateCallback }) => {
+    const [firstName, setFirstName] = useState(existingMember.firstName || "");
+    const [lastName, setLastName] = useState(existingMember.lastName || "");
+    const [email, setEmail] = useState(existingMember.email || "");
 
-  useEffect(() => {
-    usernameField.current.focus();
-  }, []);
+    const updating = Object.entries(existingMember).length !== 0
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-
-    const username = usernameField.current.value;
-    const email = emailField.current.value;
-
-    let errors ={};
-
-    if (!username) errors.username = "Username required";
-    if (!email) errors.email = "Email required";
-
-    if (Object.keys(errors).length > 0) {
-        setFormErrors(errors);
-        return;
+    const onSubmit = async (e) => {
     }
 
-    setFormErrors({});
+    return (
+      <Body sidebar>
+        <form onSubmit={onSubmit}>
+            <div>
+                <label htmlFor="firstName">First Name:</label>
+                <input
+                    type="text"
+                    id="firstName"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="lastName">Last Name:</label>
+                <input
+                    type="text"
+                    id="lastName"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <button type="submit">{updating ? "Update" : "Create"}</button>
+        </form>
+      </Body>
+    );
+};
 
-    const response = await fetch("/register", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            username: username,
-            email: email,
-        })
-    });
-
-    const data = await response.json();
-
-    console.log(data);
-
-    if (!response.ok) {
-        if (data.message.includes("username")) {
-            setFormErrors({ username: data.message });
-        } else if (data.message.includes("Email")) {
-            setFormErrors({ email: data.message });
-        } else {
-            setFormErrors({ username: data.message || "Registration failed"});
-        }
-        return;
-    }
-
-    window.location.href = "/login";
-
-  };
-
-  return (
-    <Body sidebar>
-      <h1>Register</h1>
-      <Form onSubmit={onSubmit}>
-        <InputField
-          name="username" label="Username"
-          error={formErrors.username} fieldRef={usernameField} />
-        <InputField
-          name="email" label="Email address"
-          error={formErrors.email} fieldRef={emailField} />
-        <Button variant="primary" type="submit">Register</Button>
-      </Form>
-    </Body>
-  );
-}
+export default MemberForm
