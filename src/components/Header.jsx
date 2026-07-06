6e5a55c188ca9fe1ab222b7608881986f9de67ca
luiss28 <lsanchez@wpi.edu>
2026-07-06 10:13:47 -0400

    Cropped logo image and included it in the brand header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,17 @@ export default function Header() {
   return (
     <Navbar sticky="top" className="Header">
       <Container>
-        <Navbar.Brand>
-          San Juan Bosco Grupo Juvenil
-          <div className="text-muted fs-6">
-            Parroquia San Jose y San Esteban
+        <Navbar.Brand className="d-flex align-items-center gap-3">
+          <img
+            src="/YG_Logo.jpg"
+            alt="YG logo"
+            style={{ width: '48px', height: '48px', objectFit: 'cover', borderRadius: '8px' }}
+          />
+          <div>
+            <div>San Juan Bosco Grupo Juvenil</div>
+            <div className="text-muted fs-6">
+              Parroquia San Jose y San Esteban
+            </div>
           </div>
         </Navbar.Brand>
       </Container>
